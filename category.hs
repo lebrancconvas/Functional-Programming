8404dbb0c7ad4394b8531d3d323a55143a402413
lebrancconvas <lebrancconvas@gmail.com>
2021-10-11 02:44:31 +0700

    :sparkles: Add Function to category.hs

--- category.hs
+++ category.hs
@@ -4,6 +4,10 @@ ftype x  = x + 1
 gtype :: Int -> Int -> Int
 gtype a b = a * b
 
+htype :: Int -> Int -> Bool
+htype a b = a > b
+
 main = do
   print(ftype 99)
   print(gtype 99 99) 
+  print(htype 99 98)
