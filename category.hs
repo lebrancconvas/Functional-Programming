1c131ec9b46b17381dc6ca1320991752c541833e
lebrancconvas <lebrancconvas@gmail.com>
2021-10-11 03:18:07 +0700

    :sparkles: Add Checkday in category.hs to learn about Guard Concept in Haskell.

--- category.hs
+++ category.hs
@@ -12,8 +12,18 @@ fact :: Integer -> Integer
 fact n | n == 0 = 1 
        | n /= 0 = n * fact (n-1) 
 
+checkday :: Integer -> String
+checkday numday | numday == 1 = "Sunday" 
+                | numday == 2 = "Monday" 
+                | numday == 3 = "Tuesday" 
+                | numday == 4 = "Wednesday" 
+                | numday == 5 = "Thursday" 
+                | numday == 6 = "Friday" 
+                | numday == 7 = "Saturday"
+
 main = do
   print(ftype 99)
   print(gtype 99 99) 
   print(htype 99 98)
   print(fact 5) 
+  print(checkday 2)
