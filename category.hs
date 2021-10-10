68c6100407940e38fb1432a450f2a3213633cc0b
lebrancconvas <lebrancconvas@gmail.com>
2021-10-11 02:59:15 +0700

    :sparkles: Add Factorial Function in category.hs

--- category.hs
+++ category.hs
@@ -7,7 +7,12 @@ gtype a b = a * b
 htype :: Int -> Int -> Bool
 htype a b = a > b
 
+fact :: Integer -> Integer
+fact n | n == 0 = 1 
+       | n /= 0 = n * fact (n-1) 
+
 main = do
   print(ftype 99)
   print(gtype 99 99) 
   print(htype 99 98)
+  print(fact 5) 
