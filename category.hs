5c31e956287b7c79f67eed8b2d67cb6e43a819b5
lebrancconvas <lebrancconvas@gmail.com>
2021-10-11 03:30:40 +0700

    :sparkles: Add Roots Function in category.hs to learn about Where Concept in Haskell.

--- category.hs
+++ category.hs
@@ -12,7 +12,7 @@ fact :: Integer -> Integer
 fact n | n == 0 = 1 
        | n /= 0 = n * fact (n-1) 
 
-checkday :: Integer -> String
+checkday :: Integer -> String 
 checkday numday | numday == 1 = "Sunday" 
                 | numday == 2 = "Monday" 
                 | numday == 3 = "Tuesday" 
@@ -21,9 +21,17 @@ checkday numday | numday == 1 = "Sunday"
                 | numday == 6 = "Friday" 
                 | numday == 7 = "Saturday"
 
+roots :: (Float, Float, Float) -> (Float, Float) 
+roots (a, b, c) = (x1, x2) where
+  x1 = e + sqrt(d / 2*a)
+  x2 = e - sqrt(d / 2*a)
+  d = (b*b) - (4*a*c)
+  e = -b / (2*a) 
+
 main = do
   print(ftype 99)
   print(gtype 99 99) 
   print(htype 99 98)
   print(fact 5) 
   print(checkday 2)
+  print(roots(1,2,1)) 
