e4c58f2f928c3df9a641e6694a3009c164440669
lebrancconvas <lebrancconvas@gmail.com>
2021-10-11 23:05:58 +0700

    :sparkles: Create Function to understand Associative Rule of The Category of types.

---
+++ associative.hs
@@ -0,0 +1,31 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+f :: Int -> Bool
+f x = x > 5
+
+g :: Bool -> String
+g x | x = "True"
+    | otherwise = "False"
+
+h :: String -> Integer
+h x | x == "True" = 999999999999
+    | otherwise = 111111111111
+
+hog :: Bool -> Integer
+hog x = h(g x)
+
+gof :: Int -> String
+gof x = g(f x)
+
+hogof :: Int -> Integer
+hogof x = h(gof x)
+
+hogof2 :: Int -> Integer
+hogof2 x = hog(f x)
+
+main :: IO ()
+main = do
+  let x = 99 
+  if hogof x == hogof2 x then
+    putStrLn "Associative is True."
+  else
+    putStrLn "Associative is False." 
