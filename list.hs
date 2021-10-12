43dfffcdebdc349e6be7022be7610a068dd0b29c
lebrancconvas <lebrancconvas@gmail.com>
2021-10-12 14:48:09 +0700

    :sparkles: Add More List Operation.

--- list.hs
+++ list.hs
@@ -1,9 +1,19 @@
 main :: IO ()
 main = do
   let listOfNumber = [1..10]
-  print listOfNumber 
+  print listOfNumber
   print(head listOfNumber)
   print(tail listOfNumber)
   print(last listOfNumber)
-  print(init listOfNumber) 
+  print(init listOfNumber)
+  print(null listOfNumber)
+  print(reverse listOfNumber)
+  print(length listOfNumber)
+  print(take 3 listOfNumber)
+  print(drop 2 listOfNumber)
+  print(maximum listOfNumber)
+  print(minimum listOfNumber)
+  print(sum listOfNumber)
+  print(product listOfNumber)
+  print(2 `elem` listOfNumber) 
   
