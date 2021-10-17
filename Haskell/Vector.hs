970b88c114fc571946fdc9193cb97086e3e9f841
lebrancconvas <lebrancconvas@gmail.com>
2021-10-18 02:33:07 +0700

    :sparkles: Expand 2D Vector to 3D Vector.

--- Haskell/Vector.hs
+++ Haskell/Vector.hs
@@ -1,28 +1,33 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 magnitude :: [Float] -> Float
-magnitude [x,y] = sqrt(x**2 + y**2)
+magnitude [x,y,z] = sqrt(x**2 + y**2 + z**2) 
 
 vectorangle :: [Float] -> Float
-vectorangle [x,y] = atan y/x
+vectorangle [x,y,z] = atan (z/sqrt(x**2 + y**2)) 
 
 vectcombineangle :: [Float] -> [Float] -> Float
-vectcombineangle [x1,y1] [x2,y2] = abs (vectorangle [x1,y1] - vectorangle [x2,y2]) 
+vectcombineangle [x1,y1,z1] [x2,y2,z2] = abs (vectorangle [x1,y1,z1] - vectorangle [x2,y2,z2]) 
 
 -- dotproduct :: [Float] -> [Float] -> Float
 -- dotproduct [x1,y1] [x2,y2] = magnitude [x1,y1] * magnitude [x2,y2] * cos (vectcombineangle [x1,y1] [x2,y2])
 
 dotproduct :: [Float] -> [Float] -> Float
-dotproduct [x1,y1] [x2,y2] = (x1*x2) + (y1*y2) 
+dotproduct [x1,y1,z1] [x2,y2,z2] = (x1*x2) + (y1*y2) + (z1*z2) 
 
-crossproduct :: [Float] -> [Float] -> Float
-crossproduct [x1,y1] [x2,y2] = magnitude [x1,y1] * magnitude [x2,y2] * sin (vectcombineangle [x1,y1] [x2,y2])
+crossproduct :: [Float] -> [Float] -> [Float]
+crossproduct [x1,y1,z1] [x2,y2,z2] = [y1*z2 - y2*z1, x1*z2 - x2*z1, x1*y2 - x2*y1] 
+
+crossproductmagnitude :: [Float] -> [Float] -> Float
+crossproductmagnitude [x1,y1,z1] [x2,y2,z2] = magnitude [x1,y1,z1] * magnitude [x2,y2,z2] * sin (vectcombineangle [x1,y1,z1] [x2,y2,z2])
 
 main :: IO ()
 main = do
-  let vector1 = [1,2]
-  let vector2 = [3,5] 
+  let vector1 = [1,2,5]
+  let vector2 = [3,5,10] 
+  let vector3 = [12,14,20] 
   putStrLn "Dot Product of these two vectors: "
   print(dotproduct vector1 vector2) 
   putStrLn "Cross Product of these two vectors: "
-  print(crossproduct vector1 vector2)
+  print(crossproduct vector1 vector2) 
+  print(crossproductmagnitude vector1 vector2)
