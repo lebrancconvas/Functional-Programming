31df3a0f1d22bc0fedb1411fd9d4da4a8538d592
lebrancconvas <lebrancconvas@gmail.com>
2021-10-24 19:55:11 +0700

    :sparkles: Add More Concept of Functional Programming in Javascript.

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,11 @@
-//Pure Function
+// 1.) Pure Function
 function shell(a, b) {
     console.log(a * b);
 }
 
 shell(2, 5);
 
-//Impure Function
+// 2.) Impure Function
 const adder = 5;
 
 function shellBean(a) {
@@ -14,7 +14,16 @@ function shellBean(a) {
 
 shellBean(10);
 
-//Function Return Function. 
+// 3.) First Class function
+// 3.1) Assign Function in the variable. 
+const counterRecall = function(a) {
+    return a * 1000;
+}
+
+console.log(counterRecall(2))
+
+// 4.) Higher-Order Function
+// 4.1) Function Return Function. 
 function higherOrder(a, b) {
     return function() {
         return a + b;
@@ -23,3 +32,14 @@ function higherOrder(a, b) {
 
 const higher = higherOrder(1, 2);
 console.log(higher());
+
+// 4.2) Function as arguments of Function.
+function addModifier(a, b) {
+    return a + b;
+}
+
+function multiplier(add, a, b) {
+    return add(a, b) * add(a, b);
+}
+
+console.log(multiplier(addModifier, 1, 2));
